550b7954ca2260c527d6b3e904647bc754a824d5
sadat <shameemsadat00@gmail.com>
2024-09-15 16:22:15 +0600

    update

--- secant.py
+++ secant.py
@@ -1,18 +1,19 @@
 def f(x):
     return x*x*x -2*x - 5
-def df(x):
-    return 3*x*x- 2
+
 
 def main():
-    xn,e = 2,0.0001
+    x0, x1, e = 3,16, 0.0001
 
     while True:
-        xn1 = xn - (f(xn)/df(xn))
-        print(xn, f(xn), abs(xn1-xn))
-        if(abs(xn1-xn)<e):
-            print("aprox root:", xn1)
+        
+        x2 = x1 - (f(x1)*(x1-x0))/(f(x1)-f(x0))
+        print(x0, x1, x2)
+        if(abs(x2-x1)<e):
+            print("aprox root:", x2)
             return 
-        xn = xn1
+        x0=x1
+        x1 = x2
 
 
 main()
